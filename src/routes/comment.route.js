80f6bc4e5d5c5861d15ce29793b5ee96669af175
coding-cameroon <codingcameroonweb@gmail.com>
2025-10-06 19:58:28 +0100

    comment routes added

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
 
+import {
+  createComment,
+  deleteComment,
+  getComments,
+} from "../controllers/comment.controller.js";
+import protectAuth from "../middlewares/auth.middleware.js";
+
 const commentRouter = Router();
+commentRouter.use(protectAuth);
+
+commentRouter.post("/:id", createComment);
+commentRouter.get("/:id", getComments);
+commentRouter.delete("/:id", deleteComment);
 
 export default commentRouter;
